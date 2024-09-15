26a7c9ae091454c857092d728de564810eccaf2a
SrVariable <rojohnibana@gmail.com>
2024-09-15 13:57:08 +0200

    Make ex00 more C++ alike

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,8 +5,8 @@ int	main(int argc, char **argv)
 	for (int i = 1; i < argc; ++i)
 	{
 		std::string	arg = argv[i];
-		for (std::size_t j = 0; j < arg.length(); ++j)
-			std::cout << static_cast<unsigned char>(std::toupper(arg[j]));
+		for (std::string::iterator it = arg.begin(); it != arg.end(); ++it)
+			std::cout << static_cast<unsigned char>(std::toupper(*it));
 		if (i < argc - 1)
 			std::cout << " ";
 		else
